5884ff16ba0d48940acc16ece12f5f3f3f938277
Zippybonzo <zippy@zippybonzo.xyz>
2026-06-01 21:59:17 +0100

    oops

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -27,7 +27,7 @@ wfLoadExtensions( [
     'DismissableSiteNotice',
     'Echo',
     //'EventBus',
-    //'EventLogging',
+    'EventLogging',
     //'EventStreamConfig',
     'GlobalBlocking',
     'GlobalCssJs',
